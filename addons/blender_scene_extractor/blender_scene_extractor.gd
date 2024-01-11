08f81265064d33b3473264906fa202ee4bf1eb4a
Jory Leech <joryleech@gmail.com>
2024-01-10 18:57:18 -0800

    Added mesh-library generation

--- addons/blender_scene_extractor/blender_scene_extractor.gd
+++ addons/blender_scene_extractor/blender_scene_extractor.gd
@@ -30,6 +30,9 @@ class SceneExtractorButtonsPlugin extends EditorInspectorPlugin:
 	func pressed_static_bodies():
 		generate_objects_scenes_static(node_object)
 		
+	func pressed_mesh_library():
+		generate_mesh_library(node_object)
+		
 	func generate_folder(path):
 		var dir = DirAccess.open("res://")
 		if not dir.dir_exists_absolute (path):
@@ -68,15 +71,36 @@ class SceneExtractorButtonsPlugin extends EditorInspectorPlugin:
 				var packed_scene  = PackedScene.new()
 				packed_scene.pack(child)
 				ResourceSaver.save(packed_scene, file_name)
+				
+	func generate_mesh_library(node : Node):
+		print("Generating Mesh Library")
+		var folder = node.get_scene_file_path().replace(".blend", "/")
+		generate_folder(folder)
+		var mesh_library : MeshLibrary = MeshLibrary.new()
+		for child in node.get_children():
+			if(child is MeshInstance3D):
+				print("Adding Model: "+child.name)
+				var index : int = mesh_library.get_last_unused_item_id()
+				mesh_library.create_item(
+					index
+				)
+				mesh_library.set_item_mesh(index, child.mesh)
+		var file_name = folder+node.name+"-ml.tres"
+		print(file_name)
+		var error = ResourceSaver.save(mesh_library, file_name)
+		print(error)
+		
+	func add_button(name : String, function):
+		var button = Button.new()
+		button.text = name
+		button.pressed.connect(function)
+		add_custom_control(button)
 		
 	func _parse_begin(object: Object) -> void:
 		node_object = object
-		var button = Button.new()
-		button.text = "Generate Individual Scenes"
-		button.pressed.connect(pressed)
-		add_custom_control(button)
-		var button_with_collision = Button.new()
-		button_with_collision.text = "Generate Individual Scenes (Static Bodies)"
-		button_with_collision.pressed.connect(pressed_static_bodies)
-		add_custom_control(button_with_collision)
+
+		add_button("Generate Individual Scenes", pressed)
+		add_button("Generate Individual Scenes (Static Bodies)", pressed_static_bodies)
+		add_button("Generate Mesh Library", pressed_mesh_library)
+
 
